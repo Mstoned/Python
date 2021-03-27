96e0cc0fd79a3ca8adcea35caf6ee4880486255d
Mohit Shukla <62491583+Mstoned@users.noreply.github.com>
2021-03-27 22:43:06 +0530

    Add files via upload

---
+++ Pattern/py/number2.py
@@ -0,0 +1,39 @@
+'''
+
+Print the following pattern
+Pattern for N = 4
+
+       1
+     23
+   345
+ 4567
+
+Input Format :
+
+N (Total no. of rows)
+
+Output Format :
+
+Pattern in N lines
+
+Sample Input :5
+Sample Output :
+
+          1
+        23
+      345
+    4567
+  56789
+
+ 
+ '''
+num=int(input())
+count=num
+for i in range(1,num+1):
+    while count>1:
+        print("  ",end="")
+        count=count-1
+    for j in range(0,i):
+        print(i+j,end="")
+    print()
+    count=num-i
